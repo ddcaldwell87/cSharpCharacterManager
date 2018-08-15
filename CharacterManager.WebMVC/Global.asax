c68e35d7091787df19fabdcb8730689b8dc5831e
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-08-15 09:11:29 -0400

    Add project files.

---
+++ CharacterManager.WebMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CharacterManager.WebMVC.MvcApplication" Language="C#" %>
---
+++ CharacterManager.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CharacterManager.WebMVC.Startup))]
+namespace CharacterManager.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
