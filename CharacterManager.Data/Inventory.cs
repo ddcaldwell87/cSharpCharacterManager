449dfd97846aa83bc19c30c3c9097e170695b98b
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-08-15 10:04:02 -0400

    Added Inventory properties

--- CharacterManager.Data/Inventory.cs
+++ CharacterManager.Data/Inventory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,20 @@ namespace CharacterManager.Data
 {
     class Inventory
     {
+        [Key]
+        public int InventoryID { get; set; }
+
+        public int CharacterID { get; set; }
+
+        [Required]
+        public string ItemName { get; set; }
+
+        [Required]
+        public int ItemQuantity { get; set; }
+
+        [Required]
+        public string ItemType { get; set; }
+
+        public virtual Character Character { get; set; }
     }
 }
