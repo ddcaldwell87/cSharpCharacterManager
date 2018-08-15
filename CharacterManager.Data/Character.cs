e72cf07f5b5e91c51a96305efaa60ff7eb0bf494
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-08-15 09:56:49 -0400

    Added Character properties

--- CharacterManager.Data/Character.cs
+++ CharacterManager.Data/Character.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CharacterManager.Data
 {
+    public enum Race
+    {
+        Human = 1, Elf, Dwarf, Halfling, Orc, Gnome
+    }
+
+    public enum Gender
+    {
+        Male = 1, Female
+    }
+
+    public enum Class
+    {
+        Barbarian = 1, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rogue, Sorcerer, Warlock, Wizard
+    }
+
     public class Character
     {
+        [Key]
+        public int CharacterID { get; set; }
 
+        [Required]
+        public string CharacterName { get; set; }
+
+        [Required]
+        public Race Race { get; set; }
+
+        [Required]
+        public Gender Gender { get; set; }
+
+        [Required]
+        public Class Class { get; set; }
     }
 }
