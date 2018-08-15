694fb488d4b3061bf3bc0773b67bccf7fe895106
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-08-15 10:01:26 -0400

    Added Journal properties

--- CharacterManager.Data/Journal.cs
+++ CharacterManager.Data/Journal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,22 @@ namespace CharacterManager.Data
 {
     class Journal
     {
+        [Key]
+        public int JournalID { get; set; }
+
+        public int CharacterID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+        public virtual Character Character { get; set; }
     }
 }
