56e624f73652e2b36b626a94d0e59da1cc1cd03a
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-08-21 11:53:12 -0400

    Made jumbotron fullscreen on homepage

---
+++ CharacterManager.WebMVC/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CharacterManager.WebMVC.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string CurrentViewName(this HtmlHelper html)
+        {
+            return System.IO.Path.GetFileNameWithoutExtension(
+               ((RazorView)html.ViewContext.View).ViewPath
+           );
+        }
+
+        public static string CurrentController(this HtmlHelper html)
+        {
+            return System.IO.Path.GetFileNameWithoutExtension(
+                (html.ViewContext.Controller.GetType().Name));
+        }
+    }
+}
