d875d7f1d3fdf2f4c1e6222d77045b7c6833ce72
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-08-28 16:33:22 -0400

    Refactored character details page

---
+++ CharacterManager.Data/Migrations/201808281443331_addedDisplayAnnotations.cs
@@ -0,0 +1,16 @@
+namespace CharacterManager.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedDisplayAnnotations : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
